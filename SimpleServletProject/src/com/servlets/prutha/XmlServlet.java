1ac418fb22f2939ba3f4aed78c90821c8369205e
prutha28 <prutha.bits@gmail.com>
2017-03-29 12:39:11 -0400

    Add the URL mapping for this servlet with annotation.

--- SimpleServletProject/src/com/servlets/prutha/XmlServlet.java
+++ SimpleServletProject/src/com/servlets/prutha/XmlServlet.java
@@ -1,7 +1,9 @@
 package com.servlets.prutha;
 
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 
+@WebServlet( urlPatterns = {"/xmlservlet", "/anotherservlet"})
 public class XmlServlet extends HttpServlet {
 
 }

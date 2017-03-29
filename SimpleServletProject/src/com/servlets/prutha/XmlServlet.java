158864a25f469a99aa968d2b424f028ab1116470
prutha28 <prutha.bits@gmail.com>
2017-03-29 12:39:32 -0400

    Add the doGet method.

--- SimpleServletProject/src/com/servlets/prutha/XmlServlet.java
+++ SimpleServletProject/src/com/servlets/prutha/XmlServlet.java
@@ -2,8 +2,18 @@ package com.servlets.prutha;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @WebServlet( urlPatterns = {"/xmlservlet", "/anotherservlet"})
 public class XmlServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 2342090221175341334L;
+
+	@Override
+	protected void doGet( HttpServletRequest request, 
+			HttpServletResponse response){
+		
+	}
 
 }

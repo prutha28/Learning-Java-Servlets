b45e95ff6da3d0aedf912b5a73a9dc100a79d809
prutha28 <prutha.bits@gmail.com>
2017-03-30 16:16:03 -0400

    Using Init Paramaters in the Servlet COnfig object.

--- SimpleServletProject/src/com/servlets/prutha/SimpleServlet.java
+++ SimpleServletProject/src/com/servlets/prutha/SimpleServlet.java
@@ -5,6 +5,7 @@ import java.util.Date;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -14,7 +15,11 @@ import javax.servlet.http.HttpSession;
 /**
  * Servlet implementation class SimpleServlet
  */
-@WebServlet(description = "This is a simple servlet class", urlPatterns = { "/SimpleServlet" })
+@WebServlet(
+		description = "This is a simple servlet class", 
+		urlPatterns = { "/SimpleServlet" }, 
+		initParams = { @WebInitParam( name ="applicationName", value = "My First Servlet Application")}
+		)
 public class SimpleServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -30,30 +35,34 @@ public class SimpleServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-//		response.getWriter().append("Served at: ").append(request.getContextPath());
-//		System.out.println("Request : " + request);
-//		System.out.println("Response : " + response);
-
+		// Using Http Context object.
+		// Anything set in the HTTP Context is globally available across all 
+		// servlets in teh application for all the users in any browser session/ machine
+		
+		ServletContext context = request.getServletContext();
+		context.setAttribute("postingDate", "30-March-2017");
+		String postingDateFromContext = (String) context.getAttribute("postingDate"); 
+		response.getWriter().append("\nPosting Date obtained from the context is ").append(postingDateFromContext);
+			
 		HttpSession session = request.getSession() ;
 		String username = request.getParameter("username");
 		
 		// If no username was explicitly specified in the request
 		// use the username from the session		
+	
 		if( username == null || username.length() == 0){
 			username = (String)session.getAttribute("username");
+			response.getWriter().append("\nUser Name obtained from the session is ").append(username);
+		}else{			
+			response.getWriter().append("\nUser Name obtained from the request is ").append(username);
 		}
 
 		session.setAttribute("username", username);
-		response.getWriter().append("Hello ").append(username);
-		//		response.getWriter().append("\n").append(request.toString());
 		
-		
-		// Using Http Context object.
-		
-		ServletContext context = request.getServletContext();
-		context.setAttribute("postingDate", new Date());
-		
-		
+		// Using the init parameters
+		String applicationName = this.getServletConfig().getInitParameter("applicationName");
+		response.getWriter().append("\nApplication Name  obtained from the servlet config is ").append(applicationName);
+	
 	}
 
 	/**

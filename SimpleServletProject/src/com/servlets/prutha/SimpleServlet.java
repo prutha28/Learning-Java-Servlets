11906a931ca78b5ee6942c197abc2ef296b6b142
prutha28 <prutha.bits@gmail.com>
2017-03-30 15:00:35 -0400

    Using the HttpSession

--- SimpleServletProject/src/com/servlets/prutha/SimpleServlet.java
+++ SimpleServletProject/src/com/servlets/prutha/SimpleServlet.java
@@ -6,6 +6,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class SimpleServlet
@@ -25,10 +26,23 @@ public class SimpleServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-		System.out.println("Request : " + request);
-		System.out.println("Response : " + response);
-		response.getWriter().append("\n").append(request.toString());
+
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+//		System.out.println("Request : " + request);
+//		System.out.println("Response : " + response);
+
+		HttpSession session = request.getSession() ;
+		String username = request.getParameter("username");
+		
+		// If no username was explicitly specified in the request
+		// use the username from the session		
+		if( username == null || username.length() == 0){
+			username = (String)session.getAttribute("username");
+		}
+
+		session.setAttribute("username", username);
+		response.getWriter().append("Hello ").append(username);
+		//		response.getWriter().append("\n").append(request.toString());
 	}
 
 	/**
--- SimpleServletProject/src/com/servlets/prutha/XmlServlet.java
+++ SimpleServletProject/src/com/servlets/prutha/XmlServlet.java
@@ -2,10 +2,10 @@ package com.servlets.prutha;
 
 import java.io.IOException;
 
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 //@WebServlet( urlPatterns = {"/xmlservlet", "/anotherservlet"})
 public class XmlServlet extends HttpServlet {
